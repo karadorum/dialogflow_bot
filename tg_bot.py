15f8411bb69ad191f7166a54153de1716086c33b
Oleg Blinov <boleg05@mail.ru>
2020-02-11 14:17:13 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -12,22 +12,10 @@ GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 TG_TOKEN = os.getenv('TG_TOKEN')
 PROJECT_ID = os.getenv('PROJECT_ID')
 SESSION_ID = 'current-user-id'
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-PROXY_URL = os.getenv('PROXY_URL')
 
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('tg_bot')
-
-
-REQUEST_KWARGS = {
-    'proxy_url': PROXY_URL,
-    'urllib3_proxy_kwargs': {
-        'username': USERNAME,
-        'password': PASSWORD,
-    }
-}
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -74,8 +62,7 @@ def reply_message(update, context):
 
 def main():
     """Start the bot."""
-    updater = Updater(
-        TG_TOKEN, request_kwargs=REQUEST_KWARGS, use_context=True)
+    updater = Updater(TG_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
