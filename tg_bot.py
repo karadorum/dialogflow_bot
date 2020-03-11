cd409ce44918d09afa8698d4c914986275228a6c
Oleg Blinov <contextstudio78@gmail.com>
2020-03-11 16:29:32 +0300

    Add files via upload
    added google response

--- tg_bot.py
+++ tg_bot.py
@@ -2,34 +2,27 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 import logging
 import logging.config
-import dialogflow_v2 as dialogflow
+from google_response import detect_intent_texts
 from dotenv import load_dotenv
-
-
 load_dotenv()
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+
 TG_TOKEN = os.getenv('TG_TOKEN')
-PROJECT_ID = os.getenv('PROJECT_ID')
+PROXY_URL = os.getenv('PROXY_URL')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+
 
 logger = logging.getLogger('tg_bot')
 
 
-def detect_intent_texts(session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(PROJECT_ID, session_id)
-
-    text_input = dialogflow.types.TextInput(
-        text=text, language_code=language_code)
-
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        session=session, query_input=query_input)
-
-    answer = response.query_result.fulfillment_text
-
-    return answer
+REQUEST_KWARGS = {
+    'proxy_url': PROXY_URL,
+    'urllib3_proxy_kwargs': {
+        'username': USERNAME,
+        'password': PASSWORD,
+    }
+}
 
 
 def start(update, context):
@@ -47,7 +40,7 @@ def reply_message(update, context):
 
     try:
         answer = detect_intent_texts(
-            update.message.chat_id, update.message.text, 'ru')
+            update.message.chat_id, update.message.text)
         logger.info(
             f"message:{update.message.text}, answered: {answer}")
         update.message.reply_text(answer)
@@ -58,7 +51,7 @@ def reply_message(update, context):
 def start_tg_bot():
     """Start the bot."""
     updater = Updater(
-        TG_TOKEN, use_context=True)
+        TG_TOKEN, request_kwargs=REQUEST_KWARGS, use_context=True)
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
--- vk_bot.py
+++ vk_bot.py
@@ -1,43 +1,22 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api
-import dialogflow_v2 as dialogflow
 import random
 import os
 import logging
 import logging.config
+from google_response import detect_intent_texts
 from dotenv import load_dotenv
 load_dotenv()
 
 
 VK_TOKEN = os.getenv('VK_TOKEN')
-PROJECT_ID = os.getenv('PROJECT_ID')
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 logger = logging.getLogger('vk_bot')
 
 
-def detect_intent_texts(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.types.TextInput(
-        text=text, language_code=language_code)
-
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        session=session, query_input=query_input)
-    if response.query_result.intent.is_fallback:
-        return None
-    answer = response.query_result.fulfillment_text
-    return answer
-
-
 def reply_message(event, vk_api):
     try:
-        answer = detect_intent_texts(
-            PROJECT_ID, event.user_id, event.text, 'ru')
+        answer = detect_intent_texts(event.user_id, event.text)
     except:
         logger.exception('detect intent not working')
 
@@ -60,6 +39,7 @@ def start_vk_bot():
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             reply_message(event, api)
+
 
 if __name__ == "__main__":
     start_vk_bot()
